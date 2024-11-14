4b0f1835c1832a21e0a412e1736391fce25b5583
taskinnasif144 <taskinnasif144@gmail.com>
2024-11-15 01:38:25 +0600

    added script experiment

--- src/components/GetUserInfo.tsx
+++ src/components/GetUserInfo.tsx
@@ -1,77 +1,122 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
-import Image from 'next/image'
-import React, { useState } from 'react'
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
-
-
+import Image from "next/image";
+import React, { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import ReactGA from "react-ga";
+import { sendGAEvent } from "@next/third-parties/google";
 
 const GetUserInfo = () => {
-    const [phone, setPhone] = useState('');
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [serviceType, setserviceType] = useState('Air Duct & Ventilation Cleaning');
+  const [phone, setPhone] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [serviceType, setserviceType] = useState(
+    "Air Duct & Ventilation Cleaning"
+  );
 
-    const router = useRouter()
+  const router = useRouter();
 
+  const formSubmition = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    const formSubmition = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    let form = {
+      name,
+      email,
+      phone,
+      serviceType,
+    };
 
-        let form = {
-            name,
-            email,
-            phone,
-            serviceType
-        }
-        router.push("/ThankYou");
-        setName("");
-        setEmail("");
-        setPhone("");
-        const url = process.env.NEXT_PUBLIC_BASE_URL + "/api/submit";
-        const res = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(form)
+    sendGAEvent("event", "buttonClicked", {
+      value: "ads_conversion_Book_appointment_1",
+    });
+    router.push("/ThankYou");
+    setName("");
+    setEmail("");
+    setPhone("");
+    const url = process.env.NEXT_PUBLIC_BASE_URL + "/api/submit";
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
 
-        });
+    const data = await res.json();
+  };
 
-        const data = await res.json()
+  return (
+    <div className="absolute  w-full lg:w-4/5">
+      <div className="relative p-6 rounded flex flex-col gap-6 justify-center items-center ">
+        <Image
+          alt="background image"
+          src={"/firstForm.jpeg"}
+          fill={true}
+          className="-z-10 object-cover brightness-75 rounded-2xl"
+        />
+        <h3 className="text-white font-bold  ">Get A Free Quote Now</h3>
+        <form
+          className=" flex flex-col justify-center items-center"
+          onSubmit={formSubmition}
+        >
+          <div className="flex items-center  gap-4 flex-col md:flex-row">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="px-4 py-1 rounded w-[200px] focus:outline-none"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="px-4 py-1 rounded  w-[200px] focus:outline-none"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PhoneInput
+              inputStyle={{ width: "210px" }}
+              country={"us"}
+              value={phone}
+              onChange={(value) => setPhone(value)}
+              inputClass="w-full"
+            />
+            <select
+              className="w-[210px] text-sm outline-none appearance-none text-start px-3 py-2 "
+              value={serviceType}
+              onChange={(e) => {
+                setserviceType(e.target.value);
+              }}
+            >
+              <option
+                value="Air Duct & Ventilation Cleaning"
+                className="focus:outline-none "
+              >
+                Air Duct & Ventilation Cleaning
+              </option>
+              <option
+                value="Chimney Fireplaces Cleaning"
+                className="focus:outline-none px-3 py-2"
+              >
+                Chimney Fireplaces Cleaning
+              </option>
+              <option
+                value="Dryer Vent Cleaning"
+                className="focus:outline-none px-3 py-2"
+              >
+                Dryer Vent Cleaning
+              </option>
+            </select>
+          </div>
+          <button className=" text-white bg-color-sidebar-secondary px-3 py-1 rounded-lg mt-6">
+            Submit Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-    }
-
-    return (
-        <div className='absolute  w-full lg:w-4/5'>
-            <div className='relative p-6 rounded flex flex-col gap-6 justify-center items-center '>
-                <Image alt='background image' src={"/firstForm.jpeg"} fill={true} className='-z-10 object-cover brightness-75 rounded-2xl' />
-                <h3 className='text-white font-bold  '>Get A Free Quote Now</h3>
-                <form className=' flex flex-col justify-center items-center' onSubmit={formSubmition}>
-                    <div className='flex items-center  gap-4 flex-col md:flex-row'>
-                        <input type="text" placeholder='Full Name' className='px-4 py-1 rounded w-[200px] focus:outline-none' value={name} onChange={(e) => setName(e.target.value)} />
-                        <input type="email" placeholder='Email' className='px-4 py-1 rounded  w-[200px] focus:outline-none' value={email} onChange={e => setEmail(e.target.value)} />
-                        <PhoneInput
-                            inputStyle={{ width: '210px' }}
-                            country={'us'}
-                            value={phone}
-                            onChange={value => setPhone(value)}
-                            inputClass='w-full'
-
-                        />
-                        <select className='w-[210px] text-sm outline-none appearance-none text-start px-3 py-2 ' value={serviceType} onChange={(e) => { setserviceType(e.target.value) }}>
-                            <option value="Air Duct & Ventilation Cleaning" className='focus:outline-none '>Air Duct & Ventilation Cleaning</option>
-                            <option value="Chimney Fireplaces Cleaning" className='focus:outline-none px-3 py-2'>Chimney Fireplaces Cleaning</option>
-                            <option value="Dryer Vent Cleaning" className='focus:outline-none px-3 py-2'>Dryer Vent Cleaning</option>
-                        </select>
-                    </div>
-                    <button className=' text-white bg-color-sidebar-secondary px-3 py-1 rounded-lg mt-6'>Submit Now</button>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default GetUserInfo
+export default GetUserInfo;
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,79 +1,116 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
-import Image from 'next/image'
-import React, { useState } from 'react'
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
-
+import Image from "next/image";
+import React, { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import { sendGAEvent } from "@next/third-parties/google";
 
 const Quote = () => {
-    const [phone, setPhone] = useState("");
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [Zipcode, setZipcode] = useState("");
-    const [message, setMessage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [Zipcode, setZipcode] = useState("");
+  const [message, setMessage] = useState("");
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleFormSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleFormSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        let form = {
-            name,
-            email,
-            phone,
-            Zipcode,
-            message
-        }
+    let form = {
+      name,
+      email,
+      phone,
+      Zipcode,
+      message,
+    };
+    sendGAEvent("event", "buttonClicked", {
+      value: "ads_conversion_Book_appointment_1",
+    });
 
-        router.push("/ThankYou");
-        setPhone("");
-        setEmail("");
-        setName("");
-        setZipcode("");
-        setMessage("");
+    router.push("/ThankYou");
+    setPhone("");
+    setEmail("");
+    setName("");
+    setZipcode("");
+    setMessage("");
 
-        const url = process.env.NEXT_PUBLIC_BASE_URL + "/api/submit2"
+    const url = process.env.NEXT_PUBLIC_BASE_URL + "/api/submit2";
 
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
 
-        const res = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(form)
+    const data = await res.json();
+  };
 
-        });
+  return (
+    <section
+      id="Quote"
+      className="relative text-white p-6 my-6 flex flex-col items-center"
+    >
+      <Image
+        alt="backgrod"
+        src={"/office.jpg"}
+        fill={true}
+        className="object-cover -z-10 blur-sm"
+      />
+      <h3 className="text-3xl font-bold mb-12">Get a Free Quote Today!</h3>
+      <form
+        action=""
+        className="flex flex-col gap-4 w-full max-w-[550px]"
+        onSubmit={handleFormSubmission}
+      >
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="px-3 py-1 rounded-md w-full focus:outline-none text-black "
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="px-3 py-1 rounded-md w-full focus:outline-none text-black"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="text-black">
+          <PhoneInput
+            inputStyle={{ width: "100%" }}
+            country={"us"}
+            value={phone}
+            inputProps={{ required: true }}
+            onChange={(value) => setPhone(value)}
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Zipcode"
+          className="px-3 py-1 rounded-md w-full focus:outline-none text-black"
+          value={Zipcode}
+          onChange={(e) => setZipcode(e.target.value)}
+        />
+        <textarea
+          name=""
+          id=""
+          placeholder="Message...."
+          className="px-3 py-1 rounded-md w-full focus:outline-none text-black"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button type="submit" className="bg-color-sidebar py-1 rounded-2xl">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
 
-        const data = await res.json();
-
-
-    }
-
-    return (
-        <section id='Quote' className='relative text-white p-6 my-6 flex flex-col items-center' >
-            <Image alt='backgrod' src={"/office.jpg"} fill={true} className='object-cover -z-10 blur-sm' />
-            <h3 className='text-3xl font-bold mb-12'>Get a Free Quote Today!</h3>
-            <form action="" className='flex flex-col gap-4 w-full max-w-[550px]' onSubmit={handleFormSubmission}>
-                <input type="text" placeholder='Full Name' className='px-3 py-1 rounded-md w-full focus:outline-none text-black ' value={name} onChange={e => setName(e.target.value)} />
-                <input type="email" placeholder='Email' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' value={email} onChange={e => setEmail(e.target.value)} />
-                <div className='text-black'>
-                    <PhoneInput
-                        inputStyle={{ width: '100%' }}
-                        country={'us'}
-                        value={phone}
-                        inputProps={{ required: true }}
-                        onChange={value => setPhone(value)}
-
-                    />
-                </div>
-                <input type="text" placeholder='Zipcode' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' value={Zipcode} onChange={e => setZipcode(e.target.value)} />
-                <textarea name="" id="" placeholder='Message....' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' value={message} onChange={e => setMessage(e.target.value)}></textarea>
-                <button type='submit' className='bg-color-sidebar py-1 rounded-2xl'>Submit</button>
-            </form>
-        </section>
-    )
-}
-
-export default Quote
+export default Quote;
